9f4c3e2366589315c7447fd2624e6fcd5535fd54
Nirvikar3903 <nirvikar03.k@gmail.com>
2024-07-11 09:54:48 +0530

    create middleware and function to handle possible error

--- api/index.js
+++ api/index.js
@@ -2,7 +2,8 @@ import express from'express'
 import mongoose, { connect } from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
-import authRouter from './routes/auth.route.js';
+import authRouter from './routes/auth.route.js'
+
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() => {
@@ -15,18 +16,21 @@ const app = express();
  app.use(express.json());
 
 app.listen(3000, () => {
-    console.log("Server in running on port 3000!");
+    console.log("Server in running on port 3000");
 });
 
 app.use("/api/user" , userRouter )
-app.use("/api/auth" ,authRouter )
+app.use("/api/auth" ,authRouter)
 
+//middleware for controlling errors connected to index.js 
 app.use((err,req,res,next) => {
-    const statusCode = err.statusCode || 500;
-    const message = err.message || 'Internal Server Error';
-    return res.status(statusCode).json({
-        success:false,
-        statusCode,
-        message,
-    });
-});
+     const statusCode = err.statusCode || 500;
+     const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+      success:false,
+      statusCode,
+      message,
+  });
+ });
+
+ 
--- real-estate/vite.config.js
+++ real-estate/vite.config.js
@@ -4,8 +4,14 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    port:8000,
-  },
+  // server:{
+  //   port:3000,
+  //   proxy:{
+  //     '/api' : {
+  //       target: 'http://localhost:3000',
+  //       secure:false,
+  //     },
+  //   },
+  // },
 
-})
+});
