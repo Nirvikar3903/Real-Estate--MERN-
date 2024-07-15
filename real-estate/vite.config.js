bd6dcc57ea54f987a7b59f5e93d358c1af59e450
Nirvikar3903 <nirvikar03.k@gmail.com>
2024-07-15 08:25:28 +0530

    signin signup page Functionality

--- real-estate/vite.config.js
+++ real-estate/vite.config.js
@@ -3,15 +3,16 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+
+  server:{
+    
+    proxy:{
+      '/api' :{
+        target:'http://localhost:3000',
+        secure:false ,
+      },
+    },
+  },
   plugins: [react()],
-   server:{
-     port:3000,
-     proxy:{
-       '/api' : {
-         target: 'http://localhost:3000',
-         secure:false,
-       },
-     },
-   },
 
 });
