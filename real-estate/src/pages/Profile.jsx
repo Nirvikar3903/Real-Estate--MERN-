055370b05fd4c81d3c1cfc676e9b34aca04fc185
Nirvikar3903 <nirvikar03.k@gmail.com>
2024-07-29 11:22:19 +0530

    Add delete user functionality

--- real-estate/src/pages/Profile.jsx
+++ real-estate/src/pages/Profile.jsx
@@ -11,6 +11,9 @@ import {
   updateUserStart,
   updateUserSuccess,
   updateUserFailure,
+  deleteUserFailure,
+  deleteUserStart,
+  deleteUserSuccess,
   
 } from '../redux/user/UserSlice';
 import { useDispatch } from 'react-redux';
@@ -22,6 +25,7 @@ export default function Profile() {
   const [fileUploadError, setFileUploadError] = useState(false);
   const [formData, setFormData] = useState({});
   const [updateSuccess , setUpdateSuccess] = useState(false);
+  //const [updateSuccess, setUpdateSuccess] = useState(false);
   // const [showListingsError, setShowListingsError] = useState(false);
   // const [userListings, setUserListings] = useState([]);
   const dispatch = useDispatch();
@@ -91,6 +95,27 @@ export default function Profile() {
     }
   };
 
+
+  const handleDeleteUser = async() =>{
+    try {
+      dispatch(deleteUserStart());
+      const res = await fetch(`/api/user/delete/${currentUser._id}` ,{
+        method:'DELETE',
+      });
+      const data = await res.json();
+
+      if(data.success === false){
+        dispatch(deleteUserFailure(data.message));
+        return;
+      }
+      dispatch(deleteUserSuccess(data))
+
+      
+    } catch (error) {
+      dispatch(deleteUserFailure(error.message));
+    }
+  };
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -152,12 +177,12 @@ export default function Profile() {
         </button>
       </form>
       <div className="flex justify-between mt-5">
-          <span className="text-red-700 cursor-pointer">Delete account</span>
+          <span onClick={handleDeleteUser} className="text-red-700 cursor-pointer">Delete account</span>
           <span className="text-red-700 cursor-pointer">Sign out</span>
       </div>
 
       <p className='text-red-600 mt-5'>{error ? error:''} </p>
-      <p className='text-green-700 mt-5'>{updateSuccess ? ' User updated Successfully !!!': ''}</p>
+      <p className='text-green-700 mt-5'>{updateSuccess ? ' User updated Successfully': ''}</p>
 
     </div>
   )
--- real-estate/src/redux/user/userSlice.js
+++ real-estate/src/redux/user/userSlice.js
@@ -35,10 +35,25 @@ const userSlice = createSlice({
             state.error = action.payload,
             state.loading = false;
         },
+        deleteUserStart : (state)=>{
+            state.loading = true
+        },
+        deleteUserSuccess :(state , action)=>{
+            state.currentUser = null,
+            state.loading = false,
+            state.error = null
+        },
+        deleteUserFailure :(state ,action)=>{
+            state.currentUser = action.payload,
+            state.loading = false;
+
+        }
     }
 });
 
-export const {signInStart , signInSuccess , signInFailure , 
-    updateUserStart, updateUserFailure , updateUserSuccess} = userSlice.actions ; 
+export const {
+     signInStart , signInSuccess , signInFailure , 
+     updateUserStart, updateUserFailure , updateUserSuccess ,
+     deleteUserStart,deleteUserSuccess,deleteUserFailure   } = userSlice.actions ; 
 
 export default userSlice.reducer;
